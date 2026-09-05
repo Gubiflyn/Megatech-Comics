2cacfd5cd48211f521fb0783816694a67d09ef29
AlanGitUA <alangutierrez617@gmail.com>
2026-09-05 19:51:03 -0400

    feat: crear estructura inicial de catalogo-service

---
+++ backend/catalogo-service/src/test/java/cl/megatech/catalogo/CatalogoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.megatech.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
